6bbbe3784ec234af7ab956f131d3057e4fc386fa
Amirhossein Hosseini <Blackworld.2035@gmail.com>
2018-02-07 14:51:42 +0400

    Initial Commit

---
+++ app/src/main/java/com/example/ahhosseini/rxjava2/AppConstants.java
@@ -0,0 +1,13 @@
+package com.example.ahhosseini.rxjava2;
+
+/**
+ * Created by ahHosseini on 2/2/18.
+ */
+
+public class AppConstants {
+    private AppConstants(){
+
+    }
+
+    public static final String LINE_SEPARATOR = "\n";
+}
